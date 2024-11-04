61e9729b009d04da4eb848b0b2c46e2d01bc58d4
TinyDickLion <tinydicklionsden@gmail.com>
2024-11-04 13:19:11 -0500

    ref: home styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,22 +5,16 @@ import HomePageStyles from "./css_modules/HomePageStyles.module.css";
 const HomePage = () => {
   return (
     <div className={HomePageStyles.container}>
+      {/* Background Gradient */}
+      <div className={HomePageStyles.animatedBackground}></div>
+      
       {/* Welcome Text and Start Button */}
-      <div
-        style={{
-          backgroundColor: "antiquewhite",
-          padding: "1em",
-          opacity: "0.8",
-        }}
-        className={HomePageStyles.content}
-      >
+      <div className={`${HomePageStyles.content} ${HomePageStyles.floating}`}>
         <h1 className={HomePageStyles.title}>
           Welcome to Tiny Dick Lion's Den Games!
         </h1>
-        <br></br>
         <p className={HomePageStyles.description}>
-          Embark on an exciting journey to earn rewards and reaching high
-          scores. Are you ready?
+          Embark on an exciting journey to earn rewards and reach high scores. Are you ready?
         </p>
         <div className={HomePageStyles.buttonContainer}>
           <Link to="/match-3-mania" className={HomePageStyles.button}>
